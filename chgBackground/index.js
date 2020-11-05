24c163cf72f54286723dc2eedc63a62d637fd3af
Efusanya Ayomide Emmanuel <efusanyaae@gmail.com>
2020-11-05 22:04:18 +0100

    updated index

--- chgBackground/index.js
+++ chgBackground/index.js
@@ -6,7 +6,7 @@ body.style.backgroundColor = `${colors[4]}`;
 button.addEventListener("click", myFunc);
 function myFunc() {
   let red = setTimeout(() => {
-    let indexOfColors = parseInt(Math.random() * colors.length);
+    let indexOfColors = parseInt(Math.random() * colors.length -1);
 
     body.style.backgroundColor = colors[indexOfColors];
     button.innerHTML = `<h1>The color is now ${colors[indexOfColors]} .</h1>`;
